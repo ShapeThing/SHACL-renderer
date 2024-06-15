6b866989c0f200adb7928ef2b28866419dd63972
Daniel Beeke <daniel.beeke@triply.cc>
2024-06-15 09:13:51 +0200

    Making it ssr compatible

--- lib/components/ShaclRenderer.tsx
+++ lib/components/ShaclRenderer.tsx
@@ -1,27 +1,29 @@
 import '@fontsource/roboto/latin-400.css'
-import { useEffect, useState } from 'react'
-import { MainContext, MainContextInput, MainContextProvider, initContext } from '../core/main-context'
+import { Suspense } from 'react'
+import useSWR from 'swr'
+import { MainContextInput, MainContextProvider, initContext } from '../core/main-context'
 import '../scss/style.scss'
 import NodeShape from './NodeShape'
 export * from '../core/namespaces'
 
 export type ShaclRendererProps = MainContextInput
 
-export default function ShaclRenderer(props: ShaclRendererProps) {
-  const [mainContext, setMainContext] = useState<MainContext | undefined>(undefined)
+function ShaclRendererInner(props: ShaclRendererProps) {
+  const { data: context } = useSWR('context', () => initContext(props), {
+    suspense: true
+  })
 
-  useEffect(() => {
-    initContext(props).then(setMainContext)
-  }, [])
-
-  return mainContext ? (
-    <MainContextProvider context={mainContext}>
-      <NodeShape
-        shapePointer={mainContext.shapePointer}
-        dataPointer={mainContext.dataPointer}
-        facetSearchDataPointer={mainContext.facetSearchDataPointer}
-        key="root"
-      />
+  return (
+    <MainContextProvider context={context}>
+      <NodeShape {...context} key="root" />
     </MainContextProvider>
-  ) : null
+  )
+}
+
+export default function ShaclRenderer(props: ShaclRendererProps) {
+  return (
+    <Suspense>
+      <ShaclRendererInner {...props} />
+    </Suspense>
+  )
 }
