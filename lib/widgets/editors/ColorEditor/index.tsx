600151ce0aa72fe54f4807bc95847461be8036e6
Daniel Beeke <daniel.beeke@triply.cc>
2024-10-26 23:26:46 +0200

    Adding a color field

---
+++ lib/widgets/editors/ColorEditor/index.tsx
@@ -0,0 +1,14 @@
+import factory from '@rdfjs/data-model'
+import { stsr } from '../../../core/namespaces'
+import { WidgetProps } from '../../widgets-context'
+
+export default function TextFieldEditor({ term, setTerm }: WidgetProps) {
+  return (
+    <input
+      className="input"
+      type="color"
+      value={term.value}
+      onBlur={event => setTerm(factory.literal(event.target.value, stsr('color')))}
+    />
+  )
+}
