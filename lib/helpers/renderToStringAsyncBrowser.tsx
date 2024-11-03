8f2fcdcdd4185b850f20f6375ab20b38b5d6b1f1
Daniel Beeke <daniel.beeke@triply.cc>
2024-11-03 18:53:54 +0100

    hacky fix for now for the render of data and type

--- lib/helpers/renderToStringAsyncBrowser.tsx
+++ lib/helpers/renderToStringAsyncBrowser.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+// TODO maybe use a debounce on a react 'render' event?
 export const renderToAsyncBrowser = async (children: ReactNode) => {
   const element = document.createElement('div')
   const root = createRoot(element)
@@ -10,8 +11,9 @@ export const renderToAsyncBrowser = async (children: ReactNode) => {
   const promise: Promise<string> = new Promise(resolve => {
     const checkOutput = () => {
       setTimeout(() => {
-        if (element.innerHTML) resolve(element.innerHTML)
-        else checkOutput()
+        if (element.innerHTML) {
+          setTimeout(() => resolve(element.innerHTML), 100)
+        } else checkOutput()
       }, 10)
     }
 
