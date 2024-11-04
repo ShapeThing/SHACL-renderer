b32ee4dd972d06739ab488f5e3c7b723ad12e92d
Daniel Beeke <daniel.beeke@triply.cc>
2024-11-04 08:30:59 +0100

    uri style

--- lib/widgets/viewers/URIViewer/index.tsx
+++ lib/widgets/viewers/URIViewer/index.tsx
@@ -5,9 +5,19 @@ import { WidgetProps } from '../../widgets-context'
 export default function URIViewer({ term }: WidgetProps) {
   const { jsonLdContext } = useContext(mainContext)
 
+  const labelPart = jsonLdContext
+    .compactIri(term.value)
+    .split(/\/|\#|\:/g)
+    .pop()!
+
+  const label = labelPart.split('.')[0]
+
+  const prefix = jsonLdContext.compactIri(term.value).replace(labelPart, '')
+
   return (
-    <a href={term.value} target="_blank">
-      {jsonLdContext.compactIri(term.value)}
+    <a href={term.value} title={term.value} target="_blank" className="uri">
+      {prefix.includes('://') ? '' : <span className="uri-prefix">{prefix.substring(0, prefix.length - 1)}</span>}
+      <span className="uri-label">{label}</span>
     </a>
   )
 }
