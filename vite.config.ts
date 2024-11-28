a09835ca4c2f4d3509a9fafd159a570e723bed72
Daniel Beeke <daniel.beeke@triply.cc>
2024-11-28 17:32:41 +0100

    fixing build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import { dependencies } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ['lib'] })],
   build: {
+    copyPublicDir: false,
     lib: {
       entry: './lib/index.tsx',
       name: 'ShaclRenderer',
