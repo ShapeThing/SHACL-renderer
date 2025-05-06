728293a1adbebd9ff8105a6ca3b516a71d5007d5
Daniel Beeke <daniel.beeke@triply.cc>
2025-05-06 21:29:43 +0200

    fixing storybook

--- lib/widgets/coreWidgets.ts
+++ lib/widgets/coreWidgets.ts
@@ -1,6 +1,28 @@
-import { ComponentType, LazyExoticComponent } from 'react'
-import { coreWidgetComponents, coreWidgetMetaItems } from './generated'
-import { WidgetItem, WidgetProps } from './widgets-context'
+import { ComponentType, lazy } from 'react'
+import { WidgetItem, WidgetMeta, WidgetProps } from './widgets-context'
+
+const isNode = typeof import.meta.glob !== 'function'
+
+let viteGlob
+
+if (isNode && import.meta.env?.DEV) {
+  viteGlob = async function (pattern: string, options: { eager?: boolean } = {}) {
+    const { glob } = await import('glob')
+    const files = await glob(pattern, { cwd: './lib/widgets' })
+    console.log(files)
+    const modules = files.map(async file => [
+      './' + file,
+      options.eager ? await import(/* @vite-ignore */ './' + file) : () => import(/* @vite-ignore */ './' + file)
+    ])
+
+    return Object.fromEntries(await Promise.all(modules))
+  }
+}
+
+const coreWidgetMetaItems = viteGlob
+  ? await viteGlob('./*/*/meta.ts', { eager: true })
+  : import.meta.glob('./*/*/meta.ts', { eager: true })
+const coreWidgetComponents = viteGlob ? await viteGlob('./*/*/index.tsx') : import.meta.glob('./*/*/index.tsx')
 
 export const coreWidgets: {
   editors: WidgetItem[]
@@ -23,10 +45,12 @@ for (const [path, coreWidgetMeta] of Object.entries(coreWidgetMetaItems)) {
   const componentItem = Object.entries(coreWidgetComponents).find(([innerPath]) => {
     const [, innerType, innerName] = innerPath.split('/')
     return innerType === type && innerName === name
-  })!
+  })
+
+  const widgetFetcher = componentItem![1] as () => Promise<{ default: ComponentType<Partial<WidgetProps>> }>
 
   coreWidgets[type as keyof typeof coreWidgets].push({
-    meta: coreWidgetMeta,
-    Component: componentItem[1] as LazyExoticComponent<ComponentType<Partial<WidgetProps>>>
+    meta: (coreWidgetMeta as { default: WidgetMeta }).default,
+    Component: lazy(widgetFetcher)
   })
 }
