74315342deca1cfb1e7980f0bdf22fe1a18fc7d0
Daniel Beeke <daniel.beeke@triply.cc>
2025-05-06 20:47:26 +0200

    adding back storybook plugins

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,20 +4,17 @@ const config: StorybookConfig = {
   stories: ['../README.mdx', '../**/*.mdx', '../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
   addons: [
-    // '@storybook/addon-links',
-    '@storybook/addon-essentials'
-    // '@chromatic-com/storybook',
-    // '@storybook/addon-interactions',
-    // 'storybook-addon-swc'
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    'storybook-addon-swc'
   ],
 
   typescript: {
     reactDocgen: false
   },
 
-  framework: {
-    name: '@storybook/react-vite',
-    options: {}
-  }
+  framework: '@storybook/react-vite'
 }
 export default config
