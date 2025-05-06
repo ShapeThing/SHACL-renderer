290a19154bd8f23e97da47110b0fe7822ed77aef
Daniel Beeke <daniel.beeke@triply.cc>
2025-05-06 19:19:05 +0200

    storybook build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import topLevelAwait from 'vite-plugin-top-level-await'
+import { dependencies } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [topLevelAwait(), react()],
+  // This is here for the build:storybook
+  build: {
+    copyPublicDir: false,
+    target: 'esnext',
+    rollupOptions: {
+      external: Object.keys(dependencies).filter(item => !['grapoi', 'n3', '@hydrofoil/shape-to-query'].includes(item))
+    },
+    minify: true
+  },
   resolve: {
     alias: {
       'node:events': 'events'
