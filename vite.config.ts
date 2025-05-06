8b3b8d99df13e7b3c05d16d22a6df6ffdfe15ab2
Daniel Beeke <daniel.beeke@triply.cc>
2025-05-06 18:29:49 +0200

    removing more build stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import topLevelAwait from 'vite-plugin-top-level-await'
-import { dependencies } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [topLevelAwait(), react()],
-  build: {
-    copyPublicDir: false,
-    target: 'esnext',
-    rollupOptions: {
-      external: Object.keys(dependencies).filter(item => !['grapoi', 'n3', '@hydrofoil/shape-to-query'].includes(item))
-    },
-    minify: true
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler'
-      }
-    }
-  },
   resolve: {
     alias: {
       'node:events': 'events'
